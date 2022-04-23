cec453cba9a3d35c0bad7ccfe555356b05a426cb
bruno <brunofvteixeira@gmail.com>
2022-04-23 12:32:53 -0300

    switches to firebase

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ place for storing color palettes with little bloat(?)
 ### would be nice to have
 
 - [ ] some sort of list ordering
-- [ ] real time updates
-- [ ] append only
+- [x] real time updates
+- [x] append only
+- [ ] list of (global) recent pages on index page
+- [ ] rgb picker
--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,7 +3,7 @@ import { BsArrowLeftShort } from 'react-icons/bs'
 import { FiPlus } from 'react-icons/fi'
 import Link from 'next/link'
 import { db } from 'lib/firebase'
-import { doc, onSnapshot } from 'firebase/firestore'
+import { doc, onSnapshot, setDoc } from 'firebase/firestore'
 
 import ColorBanner from 'components/ColorBanner'
 import { useRouter } from 'next/router'
@@ -11,9 +11,10 @@ import SpinningCircle from 'components/SpinningCircle'
 
 
 export default function ColorList() {
-  const [prevColors, setPrevColors] = useState<string[]>([]);
-  const [colors, setColors] = useState<string[]>([]);
+  const [serverColors, setServerColors] = useState<string[]>([]);
+  const [localColors, setLocalColors] = useState<string[]>([]);
   const [loading, setLoading] = useState(false);
+  const [initialFetch, setInitialFetch] = useState(true);
 
   const router = useRouter()
   const { slug } = router.query
@@ -21,30 +22,41 @@ export default function ColorList() {
   useEffect(() =>{
     if (slug) {
       onSnapshot(doc(db, "lists", slug as string), (doc) => {
-        setPrevColors(doc.data()?.colors)
+        if (doc.data()) {
+          setServerColors(doc.data()?.colors)
+        }
+        setInitialFetch(false)
       });
     }
   }, [slug])
 
   function changeColor(index: number, target: string) {
-    const newColors = colors
+    const newColors = localColors
     newColors[index] = target
-    setColors(newColors)
+    setLocalColors(newColors)
   }
 
-  function submitColors() {
-    const arr = prevColors.concat(colors)
+  async function submitColors() {
+    setLoading(true)
+    const newColors = serverColors.concat(localColors)
+    await setDoc(doc(db, "lists", slug as string), {
+      colors: newColors,
+    });
+    setLocalColors([])
+    setLoading(false)
   }
 
   function addColor() {
-    setColors(colors.concat(["#111"]))
+    setLocalColors(localColors.concat(["#111"]))
   }
 
+  // FIXME: quando uma cor é removida o estado fica certo e a renderizacao nao
   function removeColor(index: number) {
-    const arr = colors.slice(0, index).concat(colors.slice(index+1))
-    setColors(arr)
+    const arr = localColors.slice(0, index).concat(localColors.slice(index+1))
+    setLocalColors(arr)
   }
 
+  let serverIndex = -1
   let index = -1
   return (
     <div
@@ -53,74 +65,100 @@ export default function ColorList() {
         items-center gap-8 md:gap-12
       '
     >
-      <nav className='w-full p-4'>
+      <nav className='w-full p-4 flex justify-start'>
         <Link href="/">
-          <a href="" className='w-auto'>
+          <a href="" className=''>
             <BsArrowLeftShort size={40}/>
           </a>
         </Link>
       </nav>
       <h1
         className='
-          text-2xl
+          text-2xl font-bold
         '
+        onClick={() => console.log(localColors)}
       >
         {slug}
       </h1>
-      <div className='w-full max-w-lg flex flex-col gap-8'>
-        <div>
-          {prevColors.map((col) => {
-            index++
-            return <ColorBanner key={index} index={index} color={col} changeColor={changeColor} removeColorCallback={removeColor} disabled/>
-          })}
+      { initialFetch ?
+        <div className='inline-flex justify-center text-gray-700'>
+          <SpinningCircle />
         </div>
-        <div className=' border-t border-gray-300'>
-          {colors.map((col) => {
-            index++
-            return <ColorBanner key={index} index={index} color={col} changeColor={changeColor} removeColorCallback={removeColor}/>
-          })}
-        </div>
-      </div>
-
-      {loading ?
-        <button
-          className='
-            font-semibold
-            rounded-md
-            bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700
-            px-4 py-2
-            w-4/5 max-w-xs
-            text-white flex justify-center items-center
-          '
-        >
-          <SpinningCircle /> <span>loading...</span>
-        </button>
         :
-        <button
-          className='
-            font-semibold
-            rounded-md
-            bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700
-            px-4 py-2
-            w-4/5 max-w-xs
-            text-white flex justify-center items-center
-          '
-          onClick={submitColors}
-        >
-          save
-        </button>
-      }
+        <>
+        <div className='w-full max-w-lg flex flex-col gap-8'>
+          {/* loading */}
+          {/* server colors */}
+          <div>
+            {(serverColors.length === 0 && localColors.length === 0) &&
+              <p className='text-center text-lg'>
+                list empty
+              </p>
+            }
 
-      <button onClick={addColor}
-        className='
-          absolute bottom-8 right-8
-          h-12 w-12 rounded-full
-          flex items-center justify-center
-          text-white bg-slate-800 border border-slate-900
-        '
-      >
-        <FiPlus size={34}/>
-      </button>
+            {serverColors.map((col) => {
+              serverIndex++
+              return <ColorBanner key={serverIndex} index={index} color={col} changeColor={changeColor} removeColorCallback={removeColor} disabled/>
+            })}
+          </div>
+
+          {(serverColors.length !== 0 && localColors.length !== 0) &&
+            <hr className='border-t border-gray-300 my-4'/>
+          }
+
+          {/* local colors */}
+          { localColors.length !== 0 &&
+            <div>
+              {localColors.map((col) => {
+                index++
+                return <ColorBanner key={index} index={index} color={col} changeColor={changeColor} removeColorCallback={removeColor}/>
+              })}
+            </div>
+          }
+        </div>
+
+        {loading ?
+          <button
+            className='
+              font-semibold
+              rounded-md
+              bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700
+              px-4 py-2
+              w-4/5 max-w-xs
+              text-white flex justify-center items-center
+              gap-2
+            '
+          >
+            <SpinningCircle /><span>loading...</span>
+          </button>
+          :
+          <button
+            className='
+              font-semibold
+              rounded-md
+              bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700
+              px-4 py-2
+              w-4/5 max-w-xs
+              text-white flex justify-center items-center
+            '
+            onClick={submitColors}
+          >
+            save
+          </button>
+        }
+
+        <button onClick={addColor}
+          className='
+            absolute bottom-8 right-8
+            h-12 w-12 rounded-full
+            flex items-center justify-center
+            text-white bg-slate-800 border border-slate-900
+          '
+        >
+          <FiPlus size={34}/>
+        </button>
+        </>
+      }
     </div>
   )
 }
